b8d0a6e6d5c6195b266ce3ae221f145a3b0d6cb9
João Victor Costa Santos <rm88372@fiap.com.br>
2022-02-22 13:10:36 -0300

    Subindo projeto pro github.

---
+++ src/main/java/io/github/vcosmusjoao/clientes/ClientesApplication.java
@@ -0,0 +1,11 @@
+package io.github.vcosmusjoao.clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClientesApplication.class,args);
+    }
+}
---
+++ src/main/java/io/github/vcosmusjoao/clientes/model/entity/Cliente.java
@@ -0,0 +1,29 @@
+package io.github.vcosmusjoao.clientes.model.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//banco de dados fica responsavel por auto incrementar o id
+    private Integer id;
+
+    @Column(nullable = false,length = 150)//not null,  tamanho do nome do cliente
+    private String nome;
+
+    @Column(nullable = false,length = 11)//not null, tamanho do  cpf
+    private String cpf;
+
+    @Column
+    private LocalDate dataDoCadastro;
+
+
+
+}
