f26ba883ff91f9f2a4f6369c486597a4e77c9f07
João Victor Costa Santos <rm88372@fiap.com.br>
2022-02-24 13:42:22 -0300

    Incluindo metodo de encontrar cliente por ID

--- src/main/java/io/github/vcosmusjoao/clientes/rest/ClienteController.java
+++ src/main/java/io/github/vcosmusjoao/clientes/rest/ClienteController.java
@@ -5,6 +5,7 @@ import io.github.vcosmusjoao.clientes.model.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -16,7 +17,14 @@ public class ClienteController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente salvarCliente(@RequestBody Cliente cliente){
+    public Cliente saveCliente(@RequestBody Cliente cliente){
         return clienteRepository.save(cliente);
+    }
+
+    @GetMapping("/{id}")
+    public Cliente getCliente(@PathVariable Integer id){
+        return clienteRepository
+                .findById(id)
+                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 }
